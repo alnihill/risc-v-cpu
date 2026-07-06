d5acef87d97bdf970077eeb339c97e2b92bc994b
me <me@alexanderhill.work>
2026-07-05 22:57:08 -0700

    -g when compiling main.c

--- b.sh
+++ b.sh
@@ -1,4 +1,4 @@
-riscv64-elf-gcc -march=rv32i_zicsr -mabi=ilp32 -ffreestanding -nostdlib -c main.c -o main.o
+riscv64-elf-gcc -march=rv32i_zicsr -mabi=ilp32 -ffreestanding -nostdlib -c main.c -g -o main.o
 riscv64-elf-gcc -march=rv32i_zicsr -mabi=ilp32 -c boot.s -o boot.o
 riscv64-elf-ld -m elf32lriscv -T link.ld boot.o main.o -o main.elf
 riscv64-elf-objcopy -O binary main.elf main.bin
